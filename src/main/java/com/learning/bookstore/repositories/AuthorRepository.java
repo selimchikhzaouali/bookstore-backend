f41d53acf22085b60c4dac69401cbe70c01543e7
Selim CHIKH ZAOUALI <selim.chikhzaouali@gmail.com>
2025-01-13 18:43:32 +0100

    Adding repositories

---
+++ src/main/java/com/learning/bookstore/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.learning.bookstore.repositories;
+
+import com.learning.bookstore.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
